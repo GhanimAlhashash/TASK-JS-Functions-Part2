e04bf162a8d1faf26207aa82b8697445d3a13406
Ghanimalhashash <Ghanim-alhashash@hotmail.com>
2024-11-03 23:13:50 +0300

    functions 2

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,9 +20,12 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  if (n % 2 != 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 /**
  * oddsSmallerThan(n):
  * - receives a number n
@@ -33,7 +36,7 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  return Math.floor(n / 2);
 }
 
 /**
@@ -47,7 +50,11 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  if (n % 2 != 0) {
+    return n * n;
+  } else {
+    return n * 2;
+  }
 }
 
 module.exports = {
